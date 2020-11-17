de7217cad0126b42694c814f09617573e50f6c8d
bestming <1743635879@qq.com>
2020-11-17 23:32:20 +0800

    “这里是提交的说明”

---
+++ remove-github-repos.sh
@@ -0,0 +1,10 @@
+# ⛓项目永久链接：https://github.com/itzhangbao/remove-repos
+# 将 DELETE_KOKEN 和 GithubName 都替换为自己的
+DELETE_KOKEN="XXXXXXXX"
+GithubName="bestming"
+
+for repName in $(cat repos.txt)
+do
+    echo "Delete "$repName
+    curl -XDELETE -H "Authorization: token ${DELETE_KOKEN}" https://api.github.com/repos/${GithubName}/${repName}
+done
